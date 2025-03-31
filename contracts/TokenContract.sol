fbde7ede4633ff7ca62000912a59a43d1f60ac9c
Max Orlov <indigoram89@ya.ru>
2025-03-31 14:48:42 +0300

    Add gradual token unlocking for marketing and team tokens

--- contracts/TokenContract.sol
+++ contracts/TokenContract.sol
@@ -8,22 +8,30 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract TokenContract is ERC20, Ownable {
     using SafeERC20 for IERC20;
 
+    uint256 public constant UNLOCK_PERIOD = 30 days;
+    uint256 public constant UNLOCK_PERCENTAGE = 10;
+    uint256 public constant UNLOCK_PORTIONS = 10;
+
     uint256 public immutable liquiditySupply;
     uint256 public liquidityLeft;
 
     uint256 public immutable marketingSupply;
     uint256 public marketingLeft;
+    uint256 public immutable marketingUnlockStart;
+    uint256 public marketingUnlockedPortions;
 
     uint256 public immutable teamSupply;
     uint256 public teamLeft;
-    uint256 public immutable teamUnlockTime;
+    uint256 public immutable teamUnlockStart;
+    uint256 public teamUnlockedPortions;
 
     event LiquidityTransferred(address indexed to, uint256 amount);
     event MarketingTransferred(address indexed to, uint256 amount);
     event TeamTransferred(address indexed to, uint256 amount);
 
     constructor() ERC20("Bobe.app", "BOBE") Ownable(_msgSender()) {
-        teamUnlockTime = block.timestamp + 548 days;
+        teamUnlockStart = block.timestamp + 548 days;
+        marketingUnlockStart = block.timestamp;
 
         uint256 totalSupply = 1_000_000_000 * 10 ** 18;
         liquiditySupply = (totalSupply * 80) / 100;
@@ -34,6 +42,9 @@ contract TokenContract is ERC20, Ownable {
         marketingLeft = marketingSupply;
         teamLeft = teamSupply;
 
+        marketingUnlockedPortions = 0;
+        teamUnlockedPortions = 0;
+
         _mint(address(this), totalSupply);
     }
 
@@ -42,14 +53,40 @@ contract TokenContract is ERC20, Ownable {
         emit LiquidityTransferred(to, value);
     }
 
+    function _transferUnlockedTokens(
+        address to,
+        uint256 value,
+        uint256 unlockedAmount,
+        uint256 totalSupply,
+        uint256 tokensLeft,
+        string memory errorMessage
+    ) private onlyOwner returns (uint256) {
+        uint256 alreadyWithdrawn = totalSupply - tokensLeft;
+        uint256 availableToWithdraw = unlockedAmount - alreadyWithdrawn;
+
+        require(value <= availableToWithdraw, errorMessage);
+
+        return _transferTokens(to, value, tokensLeft);
+    }
+
     function transferMarketing(address to, uint256 value) external onlyOwner {
-        marketingLeft = _transferTokens(to, value, marketingLeft);
+        marketingLeft = _transferUnlockedTokens(
+            to,
+            value,
+            getUnlockedMarketingAmount(),
+            marketingSupply,
+            marketingLeft,
+            "Amount exceeds currently unlocked marketing tokens"
+        );
+
         emit MarketingTransferred(to, value);
     }
 
     function transferTeam(address to, uint256 value) external onlyOwner {
-        require(block.timestamp >= teamUnlockTime, "Tokens are still locked");
-        teamLeft = _transferTokens(to, value, teamLeft);
+        require(block.timestamp >= teamUnlockStart, "Team tokens are still in initial lock period");
+
+        teamLeft = _transferUnlockedTokens(to, value, getUnlockedTeamAmount(), teamSupply, teamLeft, "Amount exceeds currently unlocked team tokens");
+
         emit TeamTransferred(to, value);
     }
 
@@ -68,16 +105,111 @@ contract TokenContract is ERC20, Ownable {
         }
     }
 
-    function teamUnlockIn() external view returns (uint256) {
-        if (block.timestamp >= teamUnlockTime) return 0;
-        return teamUnlockTime - block.timestamp;
+    function _getUnlockedAmount(uint256 totalSupply, uint256 unlockStart) private view returns (uint256) {
+        if (block.timestamp < unlockStart) {
+            return 0;
+        }
+
+        uint256 elapsedPeriods = (block.timestamp - unlockStart) / UNLOCK_PERIOD;
+        if (elapsedPeriods >= UNLOCK_PORTIONS) {
+            return totalSupply;
+        }
+
+        return (totalSupply * elapsedPeriods * UNLOCK_PERCENTAGE) / 100;
     }
 
-    function _transferTokens(
-        address to,
-        uint256 value,
-        uint256 availableAmount
-    ) internal returns (uint256) {
+    function _getNextUnlock(uint256 totalSupply, uint256 unlockStart) private view returns (uint256 nextAmount, uint256 nextTimestamp) {
+        if (block.timestamp < unlockStart) {
+            return ((totalSupply * UNLOCK_PERCENTAGE) / 100, unlockStart);
+        }
+
+        uint256 elapsedPeriods = (block.timestamp - unlockStart) / UNLOCK_PERIOD;
+        if (elapsedPeriods >= UNLOCK_PORTIONS) {
+            return (0, 0);
+        }
+
+        uint256 nextPeriod = elapsedPeriods + 1;
+        uint256 nextUnlockTimestamp = unlockStart + (nextPeriod * UNLOCK_PERIOD);
+        uint256 nextTotalUnlocked = (totalSupply * nextPeriod * UNLOCK_PERCENTAGE) / 100;
+        uint256 currentlyUnlocked = (totalSupply * elapsedPeriods * UNLOCK_PERCENTAGE) / 100;
+
+        return (nextTotalUnlocked - currentlyUnlocked, nextUnlockTimestamp);
+    }
+
+    function getUnlockedMarketingAmount() public view returns (uint256) {
+        return _getUnlockedAmount(marketingSupply, marketingUnlockStart);
+    }
+
+    function getUnlockedTeamAmount() public view returns (uint256) {
+        return _getUnlockedAmount(teamSupply, teamUnlockStart);
+    }
+
+    function getNextMarketingUnlock() public view returns (uint256 nextAmount, uint256 nextTimestamp) {
+        return _getNextUnlock(marketingSupply, marketingUnlockStart);
+    }
+
+    function getNextTeamUnlock() public view returns (uint256 nextAmount, uint256 nextTimestamp) {
+        return _getNextUnlock(teamSupply, teamUnlockStart);
+    }
+
+    function teamUnlockIn() external view returns (uint256) {
+        if (block.timestamp >= teamUnlockStart) return 0;
+        return teamUnlockStart - block.timestamp;
+    }
+
+    function _getTokenUnlockInfo(
+        uint256 tokenSupply,
+        uint256 tokensLeft,
+        uint256 unlockStart
+    ) private view returns (uint256 totalUnlocked, uint256 availableToWithdraw, uint256 nextUnlockAmount, uint256 nextUnlockTime) {
+        totalUnlocked = _getUnlockedAmount(tokenSupply, unlockStart);
+        uint256 alreadyWithdrawn = tokenSupply - tokensLeft;
+        availableToWithdraw = totalUnlocked > alreadyWithdrawn ? totalUnlocked - alreadyWithdrawn : 0;
+
+        (nextUnlockAmount, nextUnlockTime) = _getNextUnlock(tokenSupply, unlockStart);
+
+        return (totalUnlocked, availableToWithdraw, nextUnlockAmount, nextUnlockTime);
+    }
+
+    function getUnlockStatus()
+        external
+        view
+        returns (
+            uint256 marketingTotalUnlocked,
+            uint256 marketingAvailableToWithdraw,
+            uint256 marketingNextUnlockAmount,
+            uint256 marketingNextUnlockTime,
+            uint256 teamTotalUnlocked,
+            uint256 teamAvailableToWithdraw,
+            uint256 teamNextUnlockAmount,
+            uint256 teamNextUnlockTime
+        )
+    {
+        (marketingTotalUnlocked, marketingAvailableToWithdraw, marketingNextUnlockAmount, marketingNextUnlockTime) = _getTokenUnlockInfo(
+            marketingSupply,
+            marketingLeft,
+            marketingUnlockStart
+        );
+
+        (teamTotalUnlocked, teamAvailableToWithdraw, teamNextUnlockAmount, teamNextUnlockTime) = _getTokenUnlockInfo(
+            teamSupply,
+            teamLeft,
+            teamUnlockStart
+        );
+
+        return (
+            marketingTotalUnlocked,
+            marketingAvailableToWithdraw,
+            marketingNextUnlockAmount,
+            marketingNextUnlockTime,
+            teamTotalUnlocked,
+            teamAvailableToWithdraw,
+            teamNextUnlockAmount,
+            teamNextUnlockTime
+        );
+    }
+
+    function _transferTokens(address to, uint256 value, uint256 availableAmount) internal returns (uint256) {
         require(to != address(0), "Invalid address");
         require(value > 0, "Value must be greater than 0");
         require(value <= availableAmount, "Not enough tokens to transfer");
